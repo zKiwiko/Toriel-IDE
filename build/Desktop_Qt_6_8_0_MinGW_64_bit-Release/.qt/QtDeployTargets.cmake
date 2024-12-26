268bee4a8c0a7547ef1db833df12e229c31bf7ec
Kiwiko <slimebomb33@gmail.com>
2024-12-26 14:14:58 -0500

    Remove build directory from version control

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Toriel-IDE_FILE C:/Users/kiwik/OneDrive/Desktop/Code-Projects/C++ Projects/Toriel-IDE/build/Desktop_Qt_6_8_0_MinGW_64_bit-Release/Toriel-IDE.exe)
-set(__QT_DEPLOY_TARGET_Toriel-IDE_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Toriel-IDE_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
